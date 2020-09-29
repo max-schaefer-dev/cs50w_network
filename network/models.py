ba7e7eeebd2a4843740e51104e200d9aed4f9736
Max Schaefer <prv.maxim@gmail.com>
2020-09-29 04:12:57 +0200

    Specification "New Post" finished

--- network/models.py
+++ network/models.py
@@ -1,6 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django import forms
 
 
 class User(AbstractUser):
     pass
+
+
+class NewPost(forms.Form):
+    text = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
+                           label='New Post', max_length=280)
+
+
+class Post(models.Model):
+    text = models.CharField(max_length=280)
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="postBy")
+
+    def __str__(self):
+        return f"User: {self.username} postet: {self.text}"
--- network/views.py
+++ network/views.py
@@ -4,11 +4,19 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, NewPost, Post
 
 
 def index(request):
-    return render(request, "network/index.html")
+    if request.method == "POST":
+        print(request.POST)
+        newPost = Post(text=request.POST["text"],
+                       username=User.objects.get(username=request.POST["user"]))
+        newPost.save()
+
+    return render(request, "network/index.html", {
+        "newPostForm": NewPost
+    })
 
 
 def login_view(request):
